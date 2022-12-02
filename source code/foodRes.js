160bc43c2f1036c04ea469c808fedbb1b0f8215e
E. Lynn <imelynnoffical@gmail.com>
2022-12-02 10:38:51 -0500

    FoodRes Update

--- source code/foodRes.js
+++ source code/foodRes.js
@@ -1,27 +1,63 @@
-async function foodResults(){
-    //how do i know which is the right link
-    const URL = `https://data.cityofnewyork.us/resource/tc6u-8rnp.json`;
-    const res = await fetch(`${URL}`);
-    const data = await res.json();
-    console.log(data);
-    if(data.res == "True") 
-    console.log(data.facility_name)
-    displayResources();
-
-
-//const res = await fetch ();
-//const data = await res.json();
-
-function displayResources(){
-    for (let i = 0; i < data.length; i++){
-    const foodRes = data[i]
-    var foodDisplay = document.getElementById("foodListCard");
-    var div = document.createElement("foodListCard");
-    div.innerHTML = `<h4 id="orgName">${foodRes.facility_name}</h4><h4 id="orgDescription"><i>Location:</i></h4>${foodRes.street_address}</h4><br>${foodRes.city}, ${foodRes.state}
-    ${foodRes.zip_code}<br><br>Phone: ${foodRes.phone_number_s_}`
-    foodDisplay.appendChild(div);
-}
-}
-}
-
-foodResults (); //calling func
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
+  function validateZip(str)
+  
+  {
+    return /^\d{5}(-\d{4})?$/.test(str);
+  }
+  
+  function validateResFood()
+  {
+   
+    let zipCode = document.getElementById("zipCodeFood").value;
+    let msg = "";
+  
+    if(validateZip(zipCode))
+    {
+      window.location.href = "foodzip.html?zip=" + zipCode; 
+    }
+   else
+     {
+       msg="Please enter a valid zip code.";
+     }
+    document.getElementById("gethelpResults").innerHTML = msg;
+    console.log("validateRes");
+  
+  
+  document.querySelector('zipCodeFood').onsubmit = e => {
+    e.target.submit();
+    e.target.reset();
+  };
+  }
+  
+  let zipCode = window.location.search.slice(5);
+  
+  fetch(`https://api.211.org/search/v1/api/Search/Keyword?Keyword=food&Location=${zipCode}&Distance=100&Top=100&OrderBy=Relevance&SearchMode=Any&IncludeStateNationalRecords=true&ReturnTaxonomyTermsIfNoResults=false`, {
+          method: 'GET',
+          // Request headers
+          headers: {
+              'Cache-Control': 'no-cache',
+              'Api-Key': '8681f8e51ffb4e1bb89ab9dad911397c',}
+      })
+      .then(response => {
+          return response.json();   
+        }).then(data => {
+            console.log(data.results);
+            for (const entry of data.results) {
+              console.log(entry.document.id)
+              var foodDisplay = document.getElementById("foodResults");
+              var div = document.createElement("foodListCard");
+             
+              div.innerHTML =  `<h4 id="orgName">${entry.document.nameOrganization}</h4><h4 id="orgDescription"></h4>${entry.document.descriptionOrganization}</h4><br><h5>Our Location:</h5>${entry.document.address1PhysicalAddress}<br>${entry.document.cityPhysicalAddress
+              }<br>${entry.document.countryPhysicalAddress}`
+              foodDisplay.appendChild(div);
+            }
+        })
+        .catch(err => console.error(err));
